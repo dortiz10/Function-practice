43552049f8c0ca41faffcbfa4674f8cf3ddc15a6
David Ortiz <113551897+dortiz10@users.noreply.github.com>
2022-11-01 20:19:41 +0000

    return practice and function practice

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
+from math import pow
 #functions printing and returning
 #print something in a fuction, its only for displaying some data, you aren't doing anything with the data
 
 #when you return in a function, you are going to use it to another part of your program
 
-apples = int(input("How many apples?"))
-oranges = int(input("How many oranges?"))
+# apples = int(input("How many apples?"))
+# oranges = int(input("How many oranges?"))
 
 
 from addfruit import add_fruit
 from subtractfruit import subtract_fruit
 from dividefruit import divide_fruit
 from displayfruit import display_fruit
-
-
+from functions import greet
+from functions import welcome
+from functions import square
+from returnfile import reverse_word
+from returnfile import usd_to_eur
+from returnfile import power
 # add fruit
-fruitAdded = add_fruit(apples,oranges) 
-print(fruitAdded) #whenever you you return items, you must put the returned value inside of a variable
+# fruitAdded = add_fruit(apples,oranges) 
+# print(fruitAdded) #whenever you you return items, you must put the returned value inside of a variable
 #subtract fruit
-subtractedFruits = subtract_fruit(apples,oranges)
-print(subtractedFruits)
+# subtractedFruits = subtract_fruit(apples,oranges)
+# print(subtractedFruits)
 # divide fruit
-dividedFruit = divide_fruit(apples,oranges)
-print(dividedFruit)
+# dividedFruit = divide_fruit(apples,oranges)
+# print(dividedFruit)
 # display the added fruit, subtracted fruit, and divided fruit
-display_fruit(fruitAdded,subtractedFruits,dividedFruit)
+# display_fruit(fruitAdded,subtractedFruits,dividedFruit)
+
+# greet()
+# welcomeName = welcome()
+# print(f"Welcome {welcomeName}")
+
+# squaredNumber = square(square)
+# square = int(pow(squaredNumber, 2))
+# print(square)
+answer = power(power)
+print(answer)
+
+reverseword = reverse_word()
+print(reverseword)
+
+usd_to_eur()
+
